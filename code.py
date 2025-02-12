faabd4e2f48fce3997c78616cf87cf84801b6852
richietaylor <richard@taylorfamily.co.za>
2025-02-12 15:11:40 +0200

    Added Skelly

---
+++ code.py
@@ -0,0 +1,11 @@
+
+import typing
+
+def reverseString(input: str) -> str:
+    return input
+
+def isPalindrome(input: str) -> bool:
+    return False
+
+def sortArray(input: typing.List[int]) -> typing.List[int]:
+    return input
